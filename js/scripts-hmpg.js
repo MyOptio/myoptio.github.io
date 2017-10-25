ffd6e66ea5341bd080d184f76b045cdd05955f6d
Erin Ruby <eruby@tenable.com>
2017-10-25 12:56:56 -0400

    First website draft

---
+++ js/scripts-hmpg.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    //jQuery smoothScroll
+    $('a').smoothScroll();
+    window.addEventListener('scroll', function() {});
+
+    //jQuery Fixed Nav
+    var mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > hdr) {
+            mn.addClass(mns);
+        } else {
+            mn.removeClass(mns);
+        }
+    });
+});
